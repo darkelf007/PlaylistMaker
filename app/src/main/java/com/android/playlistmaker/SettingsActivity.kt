16aa56d95e36e74fbeffe689919c6bd6ba6afa32
Anar <dtalievanar@gmail.com>
2024-01-08 17:38:07 +0300

    08.01.2024

--- app/src/main/java/com/android/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/android/playlistmaker/SettingsActivity.kt
@@ -1,10 +1,10 @@
 package com.android.playlistmaker
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.dental.playlistmaker.R
+
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
