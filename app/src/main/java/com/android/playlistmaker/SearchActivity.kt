365712e60f042851ca27aa16ceec6c3dd0dfd23c
Anar <dtalievanar@gmail.com>
2024-02-16 15:52:01 +0300

    16.02.2024

--- app/src/main/java/com/android/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/android/playlistmaker/SearchActivity.kt
@@ -2,76 +2,83 @@ package com.android.playlistmaker
 
 import android.app.Activity
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
+
+    private val ITunesApiBaseUrl = "https://itunes.apple.com"
+    private val retrofit = Retrofit.Builder().baseUrl(ITunesApiBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create()).build()
+    private val ITunesService = retrofit.create(iTunesAPI::class.java)
+
     private var searchText: String = ""
+
+    private val tracks = ArrayList<Track>()
+    private lateinit var trackAdapter: TrackAdapter
+    private lateinit var placeholderImage: ImageView
+    private lateinit var placeholderText: TextView
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var updateButton: Button
+    private lateinit var inputEditText: EditText
+    private lateinit var downloadIcon: ViewGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-
-        val recycler = findViewById<RecyclerView>(R.id.track_list)
-        val tracksList = ArrayList<Track>()
-        val nirvana = Track(
-            "Smells Like Teen Spirit",
-            "Nirvana",
-            293000,
-            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-        )
-        val michael = Track(
-            "Billie Jean",
-            "Michael Jackson",
-            293000,
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg",
-        )
-        val bee = Track(
-            "Stayin' Alive",
-            "Bee Gees",
-            293000,
-            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg",
-        )
-        val led = Track(
-            "Whole Lotta Love",
-            "Led Zeppelin",
-            293000,
-            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg",
-        )
-        val guns = Track(
-            "Sweet Child O'Mine",
-            "Guns N' Roses",
-            293000,
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg",
-        )
-        tracksList.add(nirvana)
-        tracksList.add(michael)
-        tracksList.add(bee)
-        tracksList.add(guns)
-        tracksList.add(led)
-        recycler.adapter = TrackAdapter(tracksList,resources)
-
-
-        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        //-------------------------------------------------------------------------------------------------------------------------------
+        trackAdapter = TrackAdapter(tracks, resources)
+        placeholderImage = findViewById(R.id.placeholderImage)
+        placeholderText = findViewById(R.id.placeholderText)
+        updateButton = findViewById(R.id.buttonUpdate)
+        inputEditText = findViewById(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
-
+        val backButton = findViewById<Button>(R.id.button_backToMain)
+        downloadIcon = findViewById(R.id.download_icon_frame)
+        recyclerView = findViewById(R.id.track_list)
+        recyclerView.adapter = trackAdapter
+        //-------------------------------------------------------------------------------------------------------------------------------
         clearButton.setOnClickListener {
+            placeholderImage.isVisible = false
+            placeholderText.isVisible = false
+            updateButton.isVisible = false
+            trackAdapter.notifyDataSetChanged()
             inputEditText.setText("")
             hideKeyboard()
+            tracks.clear()
+
         }
-        val backButton = findViewById<Button>(R.id.button_backToMain)
+
         backButton.setOnClickListener {
             finish()
         }
+
+        inputEditText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                search()
+                true
+            } else {
+                false
+            }
+        }
+
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // empty
@@ -86,6 +93,62 @@ class SearchActivity : AppCompatActivity() {
             }
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    private fun search() {
+        if (inputEditText.text.isNotEmpty()) {
+            downloadIcon.isVisible = true
+            ITunesService.search(inputEditText.text.toString())
+                .enqueue(object : Callback<TrackResponse> {
+                    override fun onResponse(
+                        call: Call<TrackResponse>, response: Response<TrackResponse>
+                    ) {
+                        downloadIcon.isVisible = false
+                        if (response.code() == 200) {
+                            tracks.clear()
+                            if (response.body()?.results?.isNotEmpty() == true) {
+                                trackAdapter.notifyDataSetChanged()
+                                tracks.addAll(response.body()?.results!!)
+                                recyclerView.isVisible = true
+                            } else {
+                                showMessage(getString(R.string.not_found))
+                            }
+                        } else {
+                            showMessage(
+                                getString(R.string.net_error)
+                            )
+                        }
+                    }
+
+                    override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                        downloadIcon.isVisible = false
+                        showMessage(getString(R.string.net_error))
+                    }
+                })
+        }
+    }
+
+    private fun showMessage(text: String) {
+        if (text.isNotEmpty()) {
+            placeholderImage.isVisible = true
+            placeholderText.isVisible = true
+            downloadIcon.isVisible = false
+            trackAdapter.notifyDataSetChanged()
+            recyclerView.isVisible = false
+            tracks.clear()
+
+            if (text == getString(R.string.not_found)) {
+                updateButton.isVisible = false
+                placeholderImage.setImageResource(R.drawable.not_found)
+                placeholderText.text = text
+            } else {
+                placeholderImage.setImageResource(R.drawable.net_error)
+                placeholderText.text = text
+                updateButton.setOnClickListener { search() }
+                updateButton.isVisible = true
+            }
+        }
+
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
--- app/src/main/java/com/android/playlistmaker/Track.kt
+++ app/src/main/java/com/android/playlistmaker/Track.kt
@@ -4,6 +4,5 @@ data class Track(
     val trackName: String, // Название композиции
     val artistName: String, // Имя исполнителя
     val trackTime: Int, // Продолжительность трека
-    val artworkUrl100: String // Ссылка на изображение обложки
-
+    val artworkUrl100: String,  // Ссылка на изображение обложки
 )
