88fce6aa2073088dff29cf4f65af778fa7200883
Anar <dtalievanar@gmail.com>
2024-09-16 18:34:23 +0300

    16.09.2024 fix

--- app/src/main/java/com/android/playlistmaker/search/data/api/ITunesAPI.kt
+++ app/src/main/java/com/android/playlistmaker/search/data/api/ITunesAPI.kt
@@ -1,11 +1,10 @@
 package com.android.playlistmaker.search.data.api
 
 import com.android.playlistmaker.search.data.model.TrackResponseData
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface iTunesAPI {
     @GET("/search?entity=song")
-    fun search(@Query("term") text: String): Call<TrackResponseData>
+    suspend fun search(@Query("term") text: String): TrackResponseData
 }
--- app/src/main/java/com/android/playlistmaker/search/data/repository/SearchDataSourceImpl.kt
+++ app/src/main/java/com/android/playlistmaker/search/data/repository/SearchDataSourceImpl.kt
@@ -2,19 +2,12 @@ package com.android.playlistmaker.search.data.repository
 
 import com.android.playlistmaker.search.data.api.iTunesAPI
 import com.android.playlistmaker.search.data.model.TrackResponseData
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+
 
 class SearchDataSourceImpl(private val iTunesService: iTunesAPI) : SearchDataSource {
 
     override suspend fun search(query: String): TrackResponseData {
-        return withContext(Dispatchers.IO) {
-            val response = iTunesService.search(query).execute()
-            if (response.isSuccessful) {
-                response.body() ?: throw Exception("Empty response body")
-            } else {
-                throw Exception("Search failed with error: ${response.code()} - ${response.message()}")
-            }
-        }
+        val response = iTunesService.search(query)
+        return response
     }
 }
